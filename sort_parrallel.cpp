9b1a5285c6fe3610d56df389de677e5f8b88d3b4
jusung lee <jusunglee@jusungs-MacBook-Pro.local>
2022-04-19 02:06:06 -0700

    not really working?

--- sort_parrallel.cpp
+++ sort_parrallel.cpp
@@ -14,7 +14,6 @@ using namespace std;
 #define DEFAULT_N "10000000000"
 #define DEFAULT_SPLIT "8"
 #define DEFAULT_SEED 123
-#define DEFAULT_CREATE "0"
 #define DEFAULT_RAM_SIZE "5"
 #define DEFAULT_THREAD_SIZE "2"
 
@@ -45,12 +44,13 @@ bool isSort(std::vector<float> arr, unsigned long N) {
   return true;
 }
 
-void sort_K(unsigned int k, unsigned long size, unsigned int start, unsigned int end) {
-  unsigned long split_size = size/k;
+void sort_K(unsigned int k, unsigned long size, unsigned int start, unsigned int end, unsigned int threads) {
+  unsigned long split_size = size/threads;
   printf("%d, %d\n", start, end);
+  printf("%lu, %lu, %d\n", split_size, size, k);
 
   for (int i = start; i < end; i++) {
-    vector<float> *readArr = new vector<float>(split_size);\
+    vector<float> *readArr = new vector<float>(split_size);
     std::string file_name = "sortedFloats_" + std::to_string(i) + ".bin";
     printf("%d\n", i);
     binRead(readArr, "randomFloats.bin", split_size,i*split_size);
@@ -60,14 +60,14 @@ void sort_K(unsigned int k, unsigned long size, unsigned int start, unsigned int
   }
 }
 
-void merge_K(unsigned int K, unsigned long size, unsigned int ram) {
-  std::vector<float> vecs[K];
+void merge_K(unsigned int K, unsigned long size, unsigned int ram, unsigned int threads) {
+  std::vector<float> vecs[K*threads];
   std::vector<float> sorted_vec;
   int index_K[K];
-  unsigned long current_MaxK[K];
-  unsigned long prev_MaxK[K];
-  unsigned long split_size = size/K;
-  unsigned long avaiable_floats = 100000000; //((ram * 1000000000)/4)/(K+1);
+  unsigned long current_MaxK[K*threads];
+  unsigned long prev_MaxK[K*threads];
+  unsigned long split_size = size/(K*threads);
+  unsigned long avaiable_floats = 10000000; //((ram * 1000000000)/4)/(K+1);
   sorted_vec.reserve(split_size);
   printf("%lu %lu\n", avaiable_floats, split_size);
   for (int i = 0; i < K; i++) {
@@ -151,8 +151,6 @@ int main(int argc, char *argv[]) {
            cxxopts::value<unsigned long>()->default_value(DEFAULT_N)},
           {"nSplit", "Number of split points",         
            cxxopts::value<unsigned long>()->default_value(DEFAULT_SPLIT)},
-          {"nCreate", "1 for creating bin file, 0 for no",         
-           cxxopts::value<unsigned int>()->default_value(DEFAULT_CREATE)},
           {"nRam", "Gb or Ram",         
           cxxopts::value<unsigned int>()->default_value(DEFAULT_RAM_SIZE)},
           {"nThreads", "Number of threads",         
@@ -161,49 +159,35 @@ int main(int argc, char *argv[]) {
   auto cl_options = options.parse(argc, argv);
   unsigned long n_size = cl_options["nSize"].as<unsigned long>();
   unsigned long n_split = cl_options["nSplit"].as<unsigned long>();
-  unsigned int n_create = cl_options["nCreate"].as<unsigned int>();
   unsigned int n_ram = cl_options["nRam"].as<unsigned int>();
   unsigned int n_threads = cl_options["nThreads"].as<unsigned int>();
 
   std::cout << "Number of floating points : " << n_size << "\n";
   std::cout << "Number of split points : " << n_split << "\n";
-  std::cout << "Create floats : " << n_create << "\n";
   std::cout << "Ram Size : " << n_ram << "\n";
   std::cout << "Number of Threads : " << n_threads << std::endl;
   
   timer serial_timer;
-
-  if (n_create == 1) {
-    // create floats
-    vector<float> *arr = new vector<float>(n_size/10);
-    for (int i = 0; i < 10; i++) {
-      arr->resize(n_size/10);
-      createFloatingPoints(arr, n_size/10, 123);
-      binWrite(arr, "randomFloats.bin", n_size/10, 1);
-      arr->resize(0);
-    }
-    delete arr;
-  }
 
   serial_timer.start();
 
   std::thread threads[n_threads];
 
   int start = 0;
-  int end = n_split/2;
+  int end = n_split;
   printf("main %d %d\n", start, end);
   for (unsigned int i = 0; i < n_threads; i++) {
     threads[i] = std::thread(sort_K, 
-                            n_split*n_threads, n_size, start, end);
+                            n_split, n_size, start, end, n_threads);
     start = end;
-    end = (i+2)*(n_split/2);          
+    end += n_split;          
     printf("main %d %d\n", start, end);
   } 
   for (int i = 0; i < n_threads; i++) {
     threads[i].join();
   } 
 
-  merge_K(n_split, n_size, n_ram);
+  merge_K(n_split, n_size, n_ram, n_threads);
 
   double time_taken = serial_timer.stop();
   printf("time: %f\n", time_taken);
